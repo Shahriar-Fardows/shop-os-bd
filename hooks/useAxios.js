29bcb931ee491bb51a13b669b5d35cdd7d4ca16d
Shahriar Fardows <shahriarfardows@gmail.com>
2026-04-25 02:00:04 +0600

    new code update

--- hooks/useAxios.js
+++ hooks/useAxios.js
@@ -3,7 +3,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: process.env.NEXT_PUBLIC_API_URL,
+    baseURL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:5000/api/v1",
     withCredentials: true, // Important for handle cookies like refreshToken
 });
 
